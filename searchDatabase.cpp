4c9f199f1dbe122b1251faeae86903edad7b2446
markfowl28 <134896933+markfowl28@users.noreply.github.com>
2026-01-13 14:40:34 -0500

    Update searchDatabase.cpp
    Prepared statements and struct implemented for viewing results

--- searchDatabase.cpp
+++ searchDatabase.cpp
@@ -2,27 +2,43 @@
 #include "Database.h"
 #include <iostream>
 #include <sqlite3.h>
+#include "Statement.h"
+#include <string>
+#include <vector>
 
-static int callback(void* NotUsed, int argc, char** argv, char** azColName) {
-	for (int i = 0; i < argc; i++) {
-		std::cout << azColName[i] << " = " << (argv[i] ? argv[i] : "NULL") << "\n";
-	}
-	std::cout << "----------------------\n";
-	return 0;
+struct User {
+	int id;
+	std::string title;
+	std::string author;
+	int available;
+};
+
+void lookupEntry(sqlite3* db) {
+	
 }
-
-void lookupEntry() {}
 
 void viewAllEntries(sqlite3* db) {
-    const char* selectSQL = "SELECT * FROM BOOKS;";
-    char* errMsg = nullptr;
+	std::vector<User> users;
 
-    if (sqlite3_exec(db, selectSQL, callback, nullptr, &errMsg) != SQLITE_OK) {
-        std::cerr << "SQL error: " << errMsg << "\n";
-        sqlite3_free(errMsg);
-    }
-}
+	std::string query = "SELECT id, author, title, available FROM BOOKS";
+    Statement stmt(db, query);
 
-	sqlite3_close(db);
+	while (sqlite3_step(stmt.get()) == SQLITE_ROW) {
+		users.push_back({
+			sqlite3_column_int(stmt.get(), 0),
+			reinterpret_cast<const char*>(sqlite3_column_text(stmt.get(), 1)),
+			reinterpret_cast<const char*>(sqlite3_column_text(stmt.get(), 2)),
+			sqlite3_column_int(stmt.get(), 3),
+			});
+	}
+
+	for (User& user : users) {
+		std::cout
+			<< "ID: " << user.id << "  "
+			<< "Title: " << user.author << "  "
+			<< "Author: " << user.title << "  "
+			<< "Available: " << user.available << "  "
+			<< "\n";
+	}
 }
 
