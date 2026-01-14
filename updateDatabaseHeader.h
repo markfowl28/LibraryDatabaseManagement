56d5b2fe426961b4b95f17a07d6fa5bf13efda92
markfowl28 <134896933+markfowl28@users.noreply.github.com>
2026-01-13 22:42:32 -0500

    Update updateDatabaseHeader.h
    Update to reflect source file

--- updateDatabaseHeader.h
+++ updateDatabaseHeader.h
@@ -1,14 +1,22 @@
 #pragma once
 #include <sqlite3.h>
 #include "Statement.h"
+#include <string>
 
 bool executeStatement(Statement&);
+
+bool updateAvailability(sqlite3*, int, bool);
+
+bool updateTitle(sqlite3*, int, const std::string&);
+
+bool updateAuthor(sqlite3*, int, const std::string&);
 
 void createEntry(sqlite3*);
 
 void editEntry(sqlite3*);
 
 void removeEntry(sqlite3*);
+
 
 
 
