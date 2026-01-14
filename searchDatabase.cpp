096a2510bd127be92b93ee3ba29b3de8330a91c0
markfowl28 <134896933+markfowl28@users.noreply.github.com>
2026-01-13 19:15:42 -0500

    Update searchDatabase.cpp
    Search option by title created; Streamlined printing function for both view all and search features

--- searchDatabase.cpp
+++ searchDatabase.cpp
@@ -5,6 +5,7 @@
 #include "Statement.h"
 #include <string>
 #include <vector>
+#include <istream>
 
 struct User {
 	int id;
@@ -13,32 +14,67 @@ struct User {
 	int available;
 };
 
-void lookupEntry(sqlite3* db) {
-	
-}
-
-void viewAllEntries(sqlite3* db) {
-	std::vector<User> users;
-
-	std::string query = "SELECT id, author, title, available FROM BOOKS";
-    Statement stmt(db, query);
+bool printResults(Statement& stmt) {
+	bool found = false;
 
 	while (sqlite3_step(stmt.get()) == SQLITE_ROW) {
-		users.push_back({
+		found = true;
+		User user{
 			sqlite3_column_int(stmt.get(), 0),
 			reinterpret_cast<const char*>(sqlite3_column_text(stmt.get(), 1)),
 			reinterpret_cast<const char*>(sqlite3_column_text(stmt.get(), 2)),
 			sqlite3_column_int(stmt.get(), 3),
-			});
-	}
+		};
 
-	for (User& user : users) {
+		std::cout << "\n";
 		std::cout
 			<< "ID: " << user.id << "  "
-			<< "Title: " << user.author << "  "
-			<< "Author: " << user.title << "  "
-			<< "Available: " << user.available << "  "
+			<< "Title: " << user.title << "  "
+			<< "Author: " << user.author << "  "
+			<< "Available: " << user.available
 			<< "\n";
 	}
+	std::cout << "\n";
+	return found;
 }
+
+
+void lookupEntry(sqlite3* db) {
+	Statement stmt(
+		db,
+		"SELECT id, title, author, available FROM BOOKS WHERE title = ? COLLATE NOCASE"
+	);
+
+	std::string book;
+	std::cout << "\n";
+	std::cout << "Please enter the title: ";
+	std::getline(std::cin >> std::ws, book);
+
+	sqlite3_bind_text(
+		stmt.get(),
+		1,
+		book.c_str(),
+		-1,
+		SQLITE_TRANSIENT
+	);
+
+	if (!printResults(stmt)) {
+		std::cout << "No books found matching that title.\n";
+		std::cout << "\n";
+	}
+}
+
+
+void viewAllEntries(sqlite3* db) {
+	std::vector<User> users;
+
+	std::string query = "SELECT id, title, author, available FROM BOOKS";
+    Statement stmt(db, query);
+
+	if (!printResults(stmt)) {
+		std::cout << "Database is empty. \n";
+		std::cout << "\n";
+	}
+}
+
 
